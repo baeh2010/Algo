06ac06f2d5709e0251631b2b2f20684c00a80913
BasilAlhakami-SonaEnterprises <baeh2010@users.noreply.github.com>
2021-10-02 20:56:18 -0700

    some simple string questions

--- Program.cs
+++ Program.cs
@@ -6,121 +6,16 @@ namespace InterviewPractise_VSCODE
     class Program
     {
 
-        
-
-  // merge intervals
-    public static int[][] IntervalIntersection(int[][] A, int[][] B) {
-     
-        List<int[]> result = new List<int[]>();
-        int i=0;
-        int j=0;
-        while(i<A.Length&&j<B.Length){
-            int lo= Math.Max(A[i][0],B[j][0]);
-            int hi=Math.Min(A[i][1],B[j][1]);
-            if(lo<=hi)
-                result.Add(new int[2]{lo,hi});
-            if(A[i][1]<B[j][1]){
-                i++;
-            }else{
-                j++;
-            }
-         
-        }
-           return result.ToArray();
-    }
-
-
-
-
-
-
+    
         static void Main(string[] args)
         {
-           /* Console.WriteLine("***shortest distance in adj List***");
-            int[][] graph = new int[5][] { new int[1] { 1 }, new int[1]{2}, new int[1] { 3 }, new int[1] { 4 }, new int[0] };
-            int start = 0;
-            int target = 4;
-           Console.WriteLine(shortestDistance(graph, start, target));
-            var path = shortestPath(graph, start, target);
-            foreach (int vertix in path)
-            {
-               Console.Write(vertix + "  ");
-            }
+         Strings strings =new Strings();
 
 
-            int[][]matrix=new int[3][]{new int[3]{0,0,0},new int[3]{1,1,0},new int[3]{1,1,0}};
-           Console.WriteLine("");
-            Console.WriteLine("***shortest distance in a binary matrix***");
-            Console.WriteLine(shortestDistance(matrix));
-            int[][]matrix2=new int[3][]{new int[3]{0,0,0},new int[3]{1,1,0},new int[3]{1,1,0}};
-           var path2 = shortestPath(matrix2);
-             foreach(int[]pair in path2){
-                Console.Write("( "+pair[0]+" , "+pair[1]+" )");
-            }
-
-        
-             Console.WriteLine("");
-          //   Console.WriteLine("****shortest path in weighted directed asyclc graph ****");
-             List<int[]>[] DAG= new List<int[]>[8];
-             DAG[0]=new List<int[]>();
-             DAG[0].Add(new int[2]{1,3});
-             DAG[0].Add(new int[2]{2,6});
-             DAG[1]= new List<int[]>();
-             DAG[1].Add(new int[2]{3,11});
-             DAG[1].Add(new int[2]{2,4});
-             DAG[1].Add(new int[2]{4,4});
-             DAG[2]=new List<int[]>();
-             DAG[2].Add(new int[2]{4,8});
-             DAG[2].Add(new int[2]{6,11});
-             DAG[3]=new List<int[]>();
-             DAG[3].Add(new int[2]{7,9});
-             DAG[4]=new List<int[]>();
-             DAG[4].Add(new int[2]{3,-4});
-             DAG[4].Add(new int[2]{5,5});
-             DAG[4].Add(new int[2]{6,2});
-             DAG[5]=new List<int[]>();
-             DAG[5].Add(new int[2]{7,1});
-             DAG[6]=new List<int[]>();
-             DAG[6].Add(new int[2]{7,2});
-             DAG[7]=new List<int[]>();
-         //  var shortestPathDAG= ShortestPathInWeightedDAG(DAG,2,6);
-         //  foreach(var s in shortestPathDAG){
-         //    Console.Write(s+"  ");
-         //  }*/
-
-        // Console.WriteLine("***coin problen o(n)***");
-
-        // Console.WriteLine(BinarySearch.BiggestCoinRow(8));
-
-//Console.WriteLine(DynamicProgramming.ClimbStairsTopDown(103));
-//Console.WriteLine(DynamicProgramming.ClimbStairsBottomUp(103));           
-  //Console.WriteLine(DynamicProgramming.TripleSteps(103));
-  //Console.WriteLine(DynamicProgramming.TripleStepsBU(103));
- // int[] inputArray=new int[3]{1,2,3};
- // Console.WriteLine(BinarySearch.FindMagicIndex(inputArray));
- // Console.WriteLine(BinarySearch.FindMagicIndexBinarySearch(inputArray));
-       /* var result= DynamicProgramming.AllSubSets(inputArray);
-        foreach(var array in result){
-            Console.Write("[ ");
-            foreach(var element in array){
-                Console.Write(element+",");
-            }
-            Console.Write("]");
-        }*/
-    /*var validP=DynamicProgramming.ValidParanthesis(3);
-    foreach(string v in validP){
-        Console.WriteLine(v+" ");
-    }*/
-      
-    
-   // int[] coins=new int[3]{1,5,25};
-  //  Console.WriteLine(DynamicProgramming.MakingChange(8,coins,new List<int>())+" ways");
-          int[][]matrix=new int[4][]{new int[4]{1,0,0,0},new int[4]{0,0,0,0},new int[4]{0,0,0,0},new int[4]{0,0,0,0}};
-  
-        var path=  Graphs.eulrianPath(matrix);
-        foreach(int[]pair in path){
-                Console.Write("( "+pair[0]+" , "+pair[1]+" )");
-            }
+         Console.WriteLine(strings.addBinary("100","1"));
+           Console.WriteLine(strings.addBinary("11","1"));
+           Console.WriteLine(strings.addBinary("1","0"));
+       
         }
   
     }
--- Strings.cs
+++ Strings.cs
@@ -1,21 +1,97 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 namespace InterviewPractise_VSCODE
 {
   public class Strings{
+        /*
+        
+         reverse all character and return result
+        */
+
+        public string reverse(string input){
+          StringBuilder sb = new StringBuilder();
+          for(int i=input.Length-1;i>=0;i--){
+            char letter=input[i];
+            sb.Append(letter);
+          }
+          return sb.ToString();
+        }
+
+        /*
+         ignoring case and non alphabetical char
+         return weather it's a palandrom
+
+
+         level
+         algorithm
+         A man, a plan, a canal: panama
+
+        */
+        public bool isPalandrom(string input){
+         int start=0;
+         int end=input.Length-1;
+         while(start<end){
+            if(!char.IsLetter(input[start])){
+              start++;
+              continue;
+            }
+            if(!char.IsLetter(input[end])){
+              end--;
+              continue;
+            }
+            if(char.ToLower(input[start++])!=char.ToLower(input[end--]))
+            return false;
+         }
+          return true;
+        }
      /*
-     car,car,dog
-     find words that start with 'ca'
-
-
-     sorting
-     bat 
-     cat
-     car 
-     dog  
-    
- 
-     tries
+        is correct capatalication 
+        it's correct if: 
+        all letters capital 
+        no letter capitalized
+        only first letter capitilized
+      
      */
+     public bool isCorrectCapitilization(string input)
+     {
+       int count=0;
+       foreach(char letter in input){
+         if(char.IsUpper(letter))
+         count++;
+       }
+       return count==input.Length||count==0||count==1&&char.IsUpper(input[0]);
+     }
+
+     /*
+      strings containing 1s and 0s
+      return their sum also a binary string 
+      neither binary string will contain leading 0 unless itself is 0
+
+      100+1 return 101
+      11+1 return 100
+      1+0 return 1 
+     */
+     public string addBinary(string num1,string num2){
+       StringBuilder sb =new StringBuilder();
+       int idx1=num1.Length-1;
+       int idx2=num2.Length-1;
+       int carry=0;
+       while(idx1>=0||idx2>=0){
+         int sum=carry;
+         if(idx1>=0){
+           sum+=num1[idx1--]-'0';
+         }
+         if(idx2>=0){
+           sum+=num2[idx2--]-'0';
+         }
+         sb.Insert(0,sum%2);
+         carry=sum/2;
+       }
+       if(carry==1)
+       sb.Insert(0,"1");
+       return sb.ToString();
+      }
+
   }
 }
